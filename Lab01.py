18a3af345708936f2614520014f30737b9cd4662
MarekAmralyUWYO <mamraly@uwyo.edu>
2024-09-12 15:55:14 -0600

    Lab01

---
+++ Lab01.py
@@ -0,0 +1,19 @@
+# Marek Amraly
+
+fname = 'Marek'
+lname = 'Amraly'
+yr = 1
+isCool = True
+pi = 3.14
+
+print('my name is', fname, lname)
+
+fname = 'bob'
+
+print('i am in year', yr, 'in college')
+
+print('am i cool?', isCool)
+
+yrplpi = yr+pi
+
+print(yrplpi)
